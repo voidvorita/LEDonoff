6a2698be051eab4e9f76034d044d6a53398f9922
ysy <tti9494@naver.com>
2019-04-30 18:01:22 +0900

    최초등록

---
+++ Inc/hc595.h
@@ -0,0 +1,11 @@
+#ifndef HC595_H_
+#define HC595_H_
+
+//#include "stm32f1xx_hal.h"
+
+#define DATA 		GPIO_PIN_0
+#define LATCH_CLOCK GPIO_PIN_1
+#define SHIFT_CLOCK	GPIO_PIN_2
+#define DATAPORT	GPIOA
+
+#endif /* 74HC595_H_ */
